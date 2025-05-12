6400dbde78cc29f7e3ccd6da3bf13ac014f43e4e
patrickytang <patrick.tang26@trinityschoolnyc.org>
2025-05-12 14:31:11 -0400

    qweqwe

--- LifeExpectancyLab/data/reformatData.py
+++ LifeExpectancyLab/data/reformatData.py
@@ -20,15 +20,20 @@ for i in range(5,66):
     ca.append(float(can[i]))
     me.append(float(mex[i]))
     us.append(float(usa[i]))
-print(ca)
+
+avg = 0
+avg2 = 0
 for i in range(4,65):
+    avg += (float(mex[i+1]) + float(usa[i+1]) + float(can[i+1]))
+    avg2+=3
     dictionary[int(years[i])] = {}
     dictionary[int(years[i])]["United States"] = float(usa[i+1])
     dictionary[int(years[i])]["Mexico"] = float(mex[i+1])
     dictionary[int(years[i])]["Canada"] = float(can[i+1])
-
-print(dictionary)
-
+    dictionary[int(years[i])]["Mean"] = (float(mex[i+1]) + float(usa[i+1]) + float(can[i+1]))/3
+    
+print(avg/avg2)
+dictionary["Meaner"] = avg/avg2
 # Create the dictionary here
 f1.close()
 
